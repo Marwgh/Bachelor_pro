1a4d42921ad4a425ecadd0c9103fc6536a88c1d8
marwane <marw0619@stud.kea.dk>
2023-05-22 21:09:11 +0200

    making login/signup pt1

--- react/src/views/signup.jsx
+++ react/src/views/signup.jsx
@@ -6,6 +6,8 @@ export default function Signup() {
 
   const onSubmit = (ev) => {
     ev.preventDefault();
+
+
   }
   return (
     <div className="signup-section">
@@ -14,11 +16,11 @@ export default function Signup() {
         <h1>
           Signup
         </h1>
-        <input type="text" name="" placeholder="First Name" id="" />
-        <input type="text" name="" placeholder="Last Name" id="" />
-        <input type="email" placeholder="email" name="" id="" />
-        <input type="password" placeholder="password" name="" id="" />
-        <input type="password" placeholder="password Confirmation" name="" id="" />
+        <input type="text" placeholder="First Name" id="" />
+        <input type="text" placeholder="Last Name" id="" />
+        <input type="email" placeholder="email" id="" />
+        <input type="password" placeholder="password" id="" />
+        <input type="password" placeholder="password Confirmation" id="" />
         <button className="signupSubBtn">Signup</button>
         <p>
           Allready registered, <Link to="/login">Sign in</Link>
